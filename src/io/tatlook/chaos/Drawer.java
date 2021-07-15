1f4bc7f49658458373739f06e9699d3ac1ac0c40
tatlook <youzhezhen@gmail.com>
2021-07-15 12:34:19 +0300

    chore: remove algs4 library
    The random number is provided by Math.random().
    Since StdArrayIO is not available,
    parsing of the .ch file is provided by the modified method.
    For these reasons, Chaos no longer needs algs4.

--- src/io/tatlook/chaos/Drawer.java
+++ src/io/tatlook/chaos/Drawer.java
@@ -97,8 +97,6 @@ import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.JComponent;
 
-import edu.princeton.cs.algs4.StdRandom;
-
 /**
  * @author Administrator
  *
@@ -333,7 +331,7 @@ public class Drawer extends JComponent implements Runnable {
 			if (sumb == 0.0) {
 				return 0;
 			}
-			double r = StdRandom.uniform(0.0, sumb);
+			double r = Math.random() * sumb;
 			for (int i = 0; i < probabilities.length; i++) {
 				sum = sum + probabilities[i];
 				if (sum > r) return i;
